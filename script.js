1d1615ad79ba7c76f59ee0b80274bbd94784e9ba
Shane Prendergast <contact@webknit.co.uk>
2015-04-09 18:40:57 +0100

    commiting new location from the api

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ var padding = 50;
 // Creates the initial SVG
 var viz = d3.select('#viz-wrapper').append('svg').attr('id', 'viz').attr('height', height).attr('width', width);
 
+
 // Get the data
-d3.json('http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&cluster=yes', function(data) {
+// http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&cluster=yes
+d3.json('http://api.openweathermap.org/data/2.5/box/city?bbox=-3,54,1,50,10&cluster=yes', function(data) {
+
+	console.log(data);
 
 	dots = viz.selectAll('circle').data(data.list).enter().append('circle');
 
@@ -15,8 +19,6 @@ d3.json('http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&clu
 	// d is the data and i the count
 	dots.attr('r', function(d, i) {
 
-		console.log(d);
-		console.log(i);
 		return Math.abs(d.main.temp);
 
 	}).attr('cx', function(d) {
@@ -36,5 +38,13 @@ d3.json('http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&clu
 		else return 'blue';
 
 	})
+
+	dots.on("mouseenter", function(d, i) {
+
+		var name = d.name;
+
+		d3.select("#name").text(name)
+
+	});
 
 });
